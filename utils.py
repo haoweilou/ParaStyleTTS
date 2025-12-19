21b6cddf9c7aa83a9cf063a380563d92588016c1
haoweilou <591193117@qq.com>
2025-12-19 16:43:13 +1100

    add more util function for training

--- utils.py
+++ utils.py
@@ -8,6 +8,12 @@ import numpy as np
 from scipy.io.wavfile import read
 import torch
 import torchaudio
+import logging
+import sys
+import glob
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging
 
 def normalize_audio(audio, target_peak=0.95):
     # Peak normalization to target_peak level (usually < 1.0)
@@ -79,6 +85,30 @@ def load_filepaths_and_text(filename, split="|"):
     filepaths_and_text = [line.strip().split(split) for line in f]
   return filepaths_and_text
 
+def load_weights(weights_path,model):
+  saved_state_dict = torch.load(weights_path, map_location='cpu')
+  if hasattr(model, 'module'):
+    state_dict = model.module.state_dict()
+  else:
+    state_dict = model.state_dict()
+  new_state_dict= {}
+  for k, v in state_dict.items():
+    try:
+      new_state_dict[k] = saved_state_dict[k]
+    except:
+      print("%s is not in the checkpoint" % k)
+      new_state_dict[k] = v
+  if hasattr(model, 'module'):
+    model.module.load_state_dict(new_state_dict)
+  else:
+    model.load_state_dict(new_state_dict)
+
+  return model
+
+def save_weights(weight):
+  weight_path = "./ckp/ParaStyleTTS_Weight.pth"
+  torch.save(weight, weight_path)
+
 def load_checkpoint(checkpoint_path, model, optimizer=None):
   assert os.path.isfile(checkpoint_path)
   checkpoint_dict = torch.load(checkpoint_path, map_location='cpu')
@@ -126,3 +156,43 @@ def resample(waveforms, orig_sr=22050, target_sr=48000):
         padded_batch[i, 0, :resampled_list[i].shape[-1]] = resampled_list[i]
 
     return padded_batch
+  
+
+def save_checkpoint(model, optimizer, learning_rate, iteration, checkpoint_path):
+  print("Saving model and optimizer state at iteration {} to {}".format(iteration, checkpoint_path))
+  if hasattr(model, 'module'):
+    state_dict = model.module.state_dict()
+  else:
+    state_dict = model.state_dict()
+  if optimizer is None:
+    return torch.save({'model': state_dict, 'iteration': iteration, 'learning_rate': learning_rate}, checkpoint_path)
+  else: 
+    return torch.save({'model': state_dict, 'iteration': iteration, 'optimizer': optimizer.state_dict(),'learning_rate': learning_rate}, checkpoint_path)
+  
+def summarize(writer, global_step, scalars={}, histograms={}, audios={}, audio_sampling_rate=22050):
+  for k, v in scalars.items():
+    writer.add_scalar(k, v, global_step)
+  for k, v in histograms.items():
+    writer.add_histogram(k, v, global_step)
+  for k, v in audios.items():
+    writer.add_audio(k, v, global_step, audio_sampling_rate)
+    
+def get_logger(model_dir, filename="train.log"):
+  global logger
+  logger = logging.getLogger(os.path.basename(model_dir))
+  logger.setLevel(logging.DEBUG)
+  
+  formatter = logging.Formatter("%(asctime)s\t%(name)s\t%(levelname)s\t%(message)s")
+  if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+  h = logging.FileHandler(os.path.join(model_dir, filename))
+  h.setLevel(logging.DEBUG)
+  h.setFormatter(formatter)
+  logger.addHandler(h)
+  return logger
+
+def latest_checkpoint_path(dir_path, regex="G_*.pth"):
+  f_list = glob.glob(os.path.join(dir_path, regex))
+  f_list.sort(key=lambda f: int("".join(filter(str.isdigit, f))))
+  x = f_list[-1]
+  return x
